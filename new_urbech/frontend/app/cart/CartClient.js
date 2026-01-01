6aef5d97ac9efa705944609a40955959e0d82c85
chamilou <chamilou@gmail.com>
2026-01-01 17:43:32 +0100

    the same '/api'

--- new_urbech/frontend/app/cart/CartClient.js
+++ new_urbech/frontend/app/cart/CartClient.js
@@ -6,7 +6,7 @@ import { useCart } from '../context/CartContext';
 import styles from './cart.module.css';
 import Link from 'next/link';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://127.0.0.1:8000/api';
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || '/api';
 
 export default function CartClient() {
   const { cart, removeFromCart, updateQuantity, getTotal, clearCart } = useCart();
